afa26ec67a31127ab8165a357aa77156206f7ed2
Thea Brick <thea@theabrick.com>
2024-02-27 23:51:11 -0500

    Fix c++ compiling issues on some platforms (#2)
    Had to copy some weird stuff from our old eternity2 interface with cadical. Specifically we need to use `libstdc++.so.6`, other things dont seem to work.
    
    Thanks james for dealing with this while i was learning lean over a year ago <3333
    
    Note: seems to be occasional race conditions when building, re-running seems to resolve. 
    
    Links for reference:
    https://github.com/JamesGallicchio/eternity2/blob/c0e868a883aabb6b53b7bc8a86434cee5f295777/lean/lakefile.lean
    https://leanprover.zulipchat.com/#narrow/stream/270676-lean4/topic/C.20FFI.20usage/near/316712266

--- lakefile.lean
+++ lakefile.lean
@@ -1,8 +1,45 @@
 import Lake
 open Lake DSL
 
+/- Copied from our old eternity2 cadical linking struggles
+  https://github.com/JamesGallicchio/eternity2/blob/c0e868a883aabb6b53b7bc8a86434cee5f295777/lean/lakefile.lean
+  https://leanprover.zulipchat.com/#narrow/stream/270676-lean4/topic/C.20FFI.20usage/near/316712266
+
+  Thanks james <333
+-/
+/-- Compute the path to `libstdc++.so.6` by running `whereis` -/
+elab "#get_libstdcpp" : command =>
+  open Lean Elab Command Term in do
+  let defLib (term : Expr) :=
+    liftTermElabM <| addAndCompile <| Declaration.defnDecl {
+        name := "libstdcpp"
+        levelParams := []
+        type := mkApp (mkConst ``Option [.zero]) (mkConst ``System.FilePath)
+        value := term
+        hints := .abbrev
+        safety := .safe
+      }
+  if System.Platform.isOSX then
+    defLib (mkApp (mkConst ``none [.zero]) (mkConst ``System.FilePath))
+    return
+  let output ← IO.Process.run {
+    cmd := "whereis"
+    args := #["libstdc++.so.6"]
+  }
+  match output.split (·.isWhitespace) with
+  | name :: loc :: _ =>
+    logInfo s!"found {name} at {loc}"
+    defLib (mkAppN (mkConst ``some [.zero]) #[
+      mkConst ``System.FilePath,
+      mkApp (mkConst ``System.FilePath.mk) (mkStrLit loc)])
+  | _ =>
+    logError ("whereis output malformed:\n" ++ output)
+
+#get_libstdcpp -- now available as `libstdcpp` declaration
+
 package «DateTime» where
   precompileModules := true
+  moreLinkArgs := match libstdcpp with | none => #[] | some x => #[x.toString]
   -- add package configuration options here
 
 lean_lib «DateTime» where
@@ -13,13 +50,13 @@ lean_exe «datetime» where
   root := `Main
   supportInterpreter := true
 
-target ffi.o pkg : FilePath := do
-  let oFile := pkg.buildDir / "DateTime" / "c" / "ffi.o"
-  let srcJob ← inputFile <| pkg.dir / "DateTime" / "c"  / "ffi.cpp"
-  let weakArgs := #["-I", (← getLeanIncludeDir).toString]
-  buildO "ffi.cpp" oFile srcJob weakArgs #["-fPIC"] "c++" getLeanTrace
+target datetime.o pkg : FilePath := do
+  let oFile := pkg.buildDir / "DateTime" / "c" / "datetime.o"
+  let srcJob ← inputFile <| pkg.dir / "DateTime" / "c"  / "datetime.cpp"
+  let flags := #["-I", (← getLeanIncludeDir).toString, "-fPIC", "-lc++"]
+  buildO "datetime.cpp" oFile srcJob flags #[] "cc"
 
-extern_lib libleanffi pkg := do
-  let name := nameToStaticLib "leanffi"
-  let ffiO ← fetch <| pkg.target ``ffi.o
-  buildStaticLib (pkg.nativeLibDir / name) #[ffiO]
+extern_lib libleandatetime pkg := do
+  let name := nameToStaticLib "datetime"
+  let datetimeO ← fetch <| pkg.target ``datetime.o
+  buildStaticLib (pkg.nativeLibDir / name) #[datetimeO]
